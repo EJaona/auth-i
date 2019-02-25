26cc67b7a251ae30e398229767fe4fa6bd317ffe
Enoka Jaona <Ejaona@gmail.com>
2019-02-25 17:35:47 -0600

    changed login to async await

--- LoginRoute.js
+++ LoginRoute.js
@@ -3,21 +3,21 @@ const bcrypt = require("bcryptjs");
 const LoginRoute = express.Router();
 const db = require("./db");
 
-LoginRoute.post("/", (req, res) => {
+LoginRoute.post("/", async (req, res) => {
   let { userName, password } = req.body;
-  db("users")
-    .where({ userName: userName })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({ message: `Welcome ${user.userName}` });
-      } else {
-        res.status(401).json({ message: "Thou shall not pass" });
-      }
-    })
-    .catch(err => {
-      res.status(500).json(err);
-    });
+  try {
+    const user = await db("users")
+      .where({ userName: userName })
+      .first();
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({ message: `Welcome ${user.userName}` });
+    } else {
+      res.status(401).json({ message: "Thou shall not pass" });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = LoginRoute;
