c289b6a0ae339dd9e8a2cf4ec87cda27afd85156
Enoka Jaona <Ejaona@gmail.com>
2019-02-26 07:59:31 -0600

    added express-session, and set up sessionConfig

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require("express");
-const knex = require("knex");
 const helmet = require("helmet");
 const UserRoute = require("./UsersRoute");
 const RegisterRoute = require("./RegisterRoute");
 const LoginRoute = require("./LoginRoute");
-const db = require("./db");
+const session = require("express-session");
+
+const sessionConfig = {
+  name: "oatmeals",
+  secret: "it's a secret, don't tell anyone",
+  cookies: {
+    maxAge: 600000,
+    security: false
+  },
+  httpOnly: true,
+  reSave: false,
+  saveUninitialized: false
+};
 
 const server = express();
 server.use(express.json());
+server.use(session(sessionConfig));
+server.use(helmet());
 server.use("/api/register", RegisterRoute);
 server.use("/api/login", LoginRoute);
 server.use("/api/users", UserRoute);
