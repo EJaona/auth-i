f6b0079e1640d75ddab46e78cffd50260c0736e6
Enoka Jaona <Ejaona@gmail.com>
2019-02-26 08:09:17 -0600

    added the use of the session object in the login request

--- LoginRoute.js
+++ LoginRoute.js
@@ -11,6 +11,7 @@ LoginRoute.post("/", async (req, res) => {
       .first();
 
     if (user && bcrypt.compareSync(password, user.password)) {
+      req.session.user = user;
       res.status(200).json({ message: `Welcome ${user.userName}` });
     } else {
       res.status(401).json({ message: "Thou shall not pass" });
