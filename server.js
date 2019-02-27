5e72ac3b9f3dedea1c828aa0c857999fff6630ab
Enoka Jaona <Ejaona@gmail.com>
2019-02-27 12:17:31 -0600

    protected middlewear is now reading reading response token from request header

--- server.js
+++ server.js
@@ -4,52 +4,22 @@ const UserRoute = require("./UsersRoute");
 const RegisterRoute = require("./RegisterRoute");
 const LoginRoute = require("./LoginRoute");
 const db = require("./db");
-// const session = require("express-session");
-// const knexSessionStore = require("connect-session-knex")(session);
-
-// const sessionConfig = {
-//   name: "oatmeals",
-//   secret: "it's a secret, don't tell anyone",
-//   cookies: {
-//     maxAge: 1000,
-//     security: false
-//   },
-//   httpOnly: true,
-//   reSave: false,
-//   saveUninitialized: false,
-//   store: new knexSessionStore({
-//     tablename: "sessions",
-//     sidfieldname: "sessionID",
-//     knex: db,
-//     createTable: true,
-//     clearInterval: 600000
-//   })
-// };
 
 const protected = (req, res, next) => {
-  next();
-  // if (req.session && req.session.user) {
-  //   next();
-  // } else {
-  //   res.status(401).json({ message: "Aint nobody got time for that" });
-  // }
+  const token = req.headers.authorization;
+
+  if (token) {
+    next();
+  } else {
+    res.status(401).json({ message: "No token provided" });
+  }
 };
 
 const server = express();
 server.use(express.json());
-// server.use(session(sessionConfig));
 server.use(helmet());
 server.use("/api/register", RegisterRoute);
 server.use("/api/login", LoginRoute);
 server.use("/api/users", protected, UserRoute);
-
-// server.get("/api/logout", async (req, res) => {
-//   if (req.session) {
-//     req.session.destroy();
-//     res.json({ message: "bye bye" });
-//   } else {
-//     res.json({ message: "You already logged out" });
-//   }
-// });
 
 module.exports = server;
